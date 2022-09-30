4fbcee0ff212bd7d06b22bd4d3ef6d1193410ca6
S203932 <s203932@student.dtu.dk>
2022-09-30 14:07:31 +0200

    Færdig oprettet test

--- spil/Main.java
+++ spil/Main.java
@@ -1,12 +1,6 @@
 package spil;
 
 public class Main {
-    // Class variables
-    private int die1;
-    private int die2;
 
-    // Die object containing two values representing each die
-    public Main(){
-        setDie();
     }
     //Hvad kalder man to gifte får? et fårhold
