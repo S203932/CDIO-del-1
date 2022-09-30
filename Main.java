8cb395421580bc67d16840ea4f8bbbe86450cf01
Fayyad2016 <114231052+Fayyad2016@users.noreply.github.com>
2022-09-30 10:36:28 +0200

    Fayyad

--- Main.java
+++ Main.java
@@ -1,25 +1,12 @@
-// Die throw between two players
+import java.math.*;
+import java.util.Random;
+public class Die {
+    // Class variables
+    private int die1;
+    private int die2;
 
-import java.util.Scanner;
+    // Die object containing two values representing the each die
+    public Die (){
+        setDie();
 
-public class Main {
-    public static void main(String[] args) {
-        Die d1, d2;
-        int sum;
-        d1=new Die();
-        d2=new Die();
-
-        Scanner m = new Scanner(System.in);
-        System.out.println("Spiler 1 kaster terninger");
-        String n=m.nextLine();
-        System.out.print(n);
-
-
-        d1.roll();
-        d2.roll();
-        sum=d1.roll()+ d2.roll();
-        System.out.println("D1:" +d1+ "; D2:" + d2);
-        System.out.println("sum:" +sum);
     }
-
-}
